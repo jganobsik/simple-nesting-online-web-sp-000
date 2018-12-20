dd0e7426522d8bb1a2d9170d23b9580f04ffd3c7
Joe Ganobsik <jganobsik@gmail.com>
2018-12-20 21:58:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     return programmer_hash[:dennis_ritchie][:languages]
+     lng = programmer_hash[:dennis_ritchie][:languages]
+     return lng
 end
 
 def adding_matz
